c67f2856a776bfe1f9254e155f838a7d073f11a8
kahboon0425 <changkahboon25@gmail.com>
2025-05-06 11:20:07 +0800

    Highlight nav bar category when selected

--- src/main.rs
+++ src/main.rs
@@ -142,6 +142,16 @@ fn Portfolio() -> impl IntoView {
         "Tree",
         "Vendor Machine",
     ];
+    let (clicked, set_clicked) = signal(0);
+
+    const CATEGORIES: &[&str] = &[
+        "All",
+        "Character Design",
+        "CGI",
+        "Animation",
+        "3D Booth Design",
+        "2D Art",
+    ];
 
     view! {
         <main class="bg-white h-full px-10 py-10">
@@ -152,21 +162,30 @@ fn Portfolio() -> impl IntoView {
 
             <div class="flex justify-center border-b border-gray-200 mt-5">
               <nav class="flex gap-8">
-                <a href="#" class="text-gray-500 hover:text-gray-700 px-3 py-2 text-sm font-medium active:border-blue-600 active:border-b-2">
-                  Character Design
-                </a>
-                <a href="#" class="text-gray-500 hover:text-gray-700 active:border-blue-600 active:border-b-2 px-3 py-2 text-sm font-medium active:border-blue-600 active:border-b-2">
-                  CGI
-                </a>
-                <a href="#" class="text-gray-500 hover:text-gray-700 px-3 py-2 text-sm font-medium active:border-blue-600 active:border-b-2">
-                  Animation
-                </a>
-                <a href="#" class="text-gray-500 hover:text-gray-700 px-3 py-2 text-sm font-medium active:border-blue-600 active:border-b-2">
-                  3D Booth Design
-                </a>
-                <a href="#" class="text-gray-500 hover:text-gray-700 px-3 py-2 text-sm font-medium active:border-blue-600 active:border-b-2">
-                  2D Art
-                </a>
+
+                {
+
+                CATEGORIES.iter().enumerate().map(|(i, f)| view!{
+                    <a href="" class=move || {
+                    let base = "text-gray-500 hover:text-gray-700 px-3 py-2 text-sm font-medium";
+                    if clicked.get() == i {
+                        format!("{base} border-blue-600 border-b-2")
+                    } else {
+                        base.to_string()
+                    }
+                    }
+
+                    on:click=move |_| {
+                        set_clicked.set(i);
+                    }
+
+                    >
+                        {*f}
+                    </a>
+                }).collect::<Vec<_>>()
+
+                }
+
               </nav>
             </div>
 
